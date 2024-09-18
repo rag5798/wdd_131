24ca5027514469f2bd22c928d9e8224c24b4fe6a
rag5798 <bgoettman@yahoo.com>
2024-09-18 15:15:26 -0600

    added week one acivity 1, 2, started week 1 prove (mission)

---
+++ week1/activity2.js
@@ -0,0 +1,7 @@
+const myapple = 3
+const friendapple = 5
+let total = myapple + friendapple
+
+document.getElementById("myAppleElement").textContent = myapple
+document.getElementById("friendAppleElement").textContent = friendapple
+document.getElementById("totalApplesElement").textContent = total
---
+++ week1/running.js
@@ -0,0 +1,6 @@
+const apples = 3
+const organges = 5
+
+let total = apples + organges
+
+console.log("Total: ", total)
