05764175df0a89d5b42ad46b4d8a4495d65e7c80
rag5798 <bgoettman@yahoo.com>
2024-09-21 17:06:52 -0600

    added element to runnning javascript

--- week1/running.js
+++ week1/running.js
@@ -4,3 +4,11 @@ const organges = 5
 let total = apples + organges
 
 console.log("Total: ", total)
+
+const h1 = document.createElement('h1')
+h1.textContent = `Apples: ${apples} Oranges: ${organges}`
+document.body.append(h1)
+
+const h2 = document.createElement('h2')
+h2.textContent = `Total: ${total}`
+document.body.append(h2)
